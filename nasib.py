8e106136c0f78355479cbb164e309834be73981d
zuna2005 <nasiba.zulunova@gmail.com>
2022-01-30 12:34:15 +0600

    ok

--- nasib.py
+++ nasib.py
@@ -19,12 +19,13 @@ bullets = pygame.sprite.Group()
 
 
 class Enemy(pygame.sprite.Sprite):
-    enemy_pic = load_image("meteor1.png")
-
     def __init__(self):
         super().__init__()
-        self.image = Enemy.enemy_pic
-        self.rect = self.image.get_rect()
+        self.frames = []
+        self.cut_sheet(load_image("meteors1.png"), 5, 1)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.count = 0
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.x = random.randrange(width)
         self.rect.y = -1 * self.image.get_height()
@@ -33,11 +34,24 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.x = random.randrange(width)
         self.life = 1
 
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
     def update(self):
         if pygame.sprite.spritecollideany(self, bullets, pygame.sprite.collide_mask):
             self.life -= 1
         if self.life > 0:
             self.rect = self.rect.move(0, 1)
+            self.count += 1
+            if self.count % 7 == 0:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
         else:
             self.kill()
 
@@ -48,7 +62,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 500, 500 # other parameters may be set in the main game
+    size = width, height = 500, 600 # other parameters may be set in the main game
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     fps = 60
